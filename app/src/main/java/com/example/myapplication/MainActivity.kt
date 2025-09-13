e7b033681eadbdde3b0c470f6744527164addcc3
JunYoung <kingjjy@gmail.com>
2025-09-13 21:57:05 +0900

    chore: auto-generate code (2025-09-13 21:57:05)

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -15,24 +15,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        textView = findViewById(R.id.textView)
+
         val buttonJunyoung = findViewById<Button>(R.id.buttonJunyoung)
         val buttonJihye = findViewById<Button>(R.id.buttonJihye)
         val buttonYuju = findViewById<Button>(R.id.buttonYuju)
-        textView = findViewById(R.id.textView)
 
         buttonJunyoung.setOnClickListener {
             countJunyoung++
-            textView.text = "박지혜 사랑해\n버튼 ${countJunyoung}번 누름"
+            textView.text = "박지혜 사랑해\n버튼 $countJunyoung번 누름"
         }
 
         buttonJihye.setOnClickListener {
             countJihye++
-            textView.text = "장준영 사랑해\n버튼 ${countJihye}번 누름"
+            textView.text = "장준영 사랑해\n버튼 $countJihye번 누름"
         }
 
         buttonYuju.setOnClickListener {
             countYuju++
-            textView.text = "코야하기 싫어어\n버튼 ${countYuju}번 누름"
+            textView.text = "코야하기 싫어어\n버튼 $countYuju번 누름"
         }
     }
 }
