ba1c080dd745a0cf40b5104c697323e005996f8c
JunYoung <kingjjy@gmail.com>
2025-09-13 17:19:25 +0900

    fix: add MainActivity.kt in correct package path

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -6,13 +6,10 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // 반드시 activity_main.xml을 사용
         setContentView(R.layout.activity_main)
 
-        // XML에 있는 id와 정확히 맞춰서 가져오기
         val button: Button = findViewById(R.id.button)
         val textView: TextView = findViewById(R.id.textView)
 
